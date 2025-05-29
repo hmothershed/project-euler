c1d645f499956564ede0d2b69bc07a89b8e0e600
Harmony Mothershed <112271331+hmothershed@users.noreply.github.com>
2025-05-29 16:46:00 -0500

    10001st prime

---
+++ python/007.py
@@ -0,0 +1,26 @@
+"""
+10 001ST PRIME
+Problem 7
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10001st prime number?
+"""
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range (2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+        
+def find_nth_prime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+    
+print(find_nth_prime(10001))
